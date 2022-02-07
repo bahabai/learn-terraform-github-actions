fc78da96fa9b103b80368535a4a4c96fe5c48e98
bahabai <73458635+bahabai@users.noreply.github.com>
2022-02-07 16:55:48 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "Bakhytzhan-Terraform-Labs"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-github-actions"
     }
   }
 }
